10010c8b218dc09d3d6873b39912413dc5db3289
John Feminella <johnf@bitsbuilder.com>
2010-10-23 18:08:04 -0400

    Add `articles` helper

--- lib/default.rb
+++ lib/default.rb
@@ -3,3 +3,10 @@
 #
 
 include BitsBuilder::Blog::Stub
+include Nanoc3::Helpers::Blogging
+include Nanoc3::Helpers::LinkTo
+include Nanoc3::Helpers::Rendering
+
+def articles
+  @items.select { |i| i[:kind] == 'article' }
+end
