65d37ea76fd2e8c1c1af59f356eb0a3ba4b29dd3
John Feminella <johnf@bitsbuilder.com>
2010-10-24 16:01:59 -0400

    Treat /posts/* content as articles

--- lib/default.rb
+++ lib/default.rb
@@ -3,10 +3,11 @@
 #
 
 include BitsBuilder::Blog::Stub
+include BitsBuilder::Blog::Post
 include Nanoc3::Helpers::Blogging
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 
 def articles
-  @items.select { |i| i[:kind] == 'article' }
+  @items.select { |i| i.article? }
 end
