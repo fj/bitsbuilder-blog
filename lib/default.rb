d56e79c61f197727432b238fe1a3cbda61d7d951
John Feminella <johnf@bitsbuilder.com>
2010-10-24 23:49:29 -0400

    DRYer Rules and more delegation of responsibility to helpers

--- lib/default.rb
+++ lib/default.rb
@@ -16,13 +16,85 @@ def log(msg)
   puts "                  ---  #{msg}"
 end
 
+class Route
+  attr_accessor :components
+  attr_accessor :name
+
+  def initialize(name, *pieces)
+    self.components ||= pieces
+    self.name       ||= name
+  end
+
+  def route
+    self.components.flatten.join('/').tap do |route|
+      log("routing `#{route}`")
+    end
+  end
+
+  def raw_route
+    self.components.map { |c| c == name ? [c, 'raw'] : c }.flatten.join('/').tap do |route|
+      log("routing raw `#{route}`")
+    end
+  end
+end
+
+module RouteHelpers
+  def route_for_article(item)
+    route_for_post(item)
+  end
+
+  def route_for_post(item)
+    prefix = 'posts'
+
+    t = Time.parse(item[:created_at])
+    entry_year  = t.strftime("%Y")
+    entry_month = t.strftime("%m")
+    entry_day   = t.strftime("%d")
+
+    date = [entry_year, entry_month, entry_day]
+
+    name = "#{item.stub}"
+
+    suffix = "index.#{item[:extension].split('.').first}"
+
+    Route.new(name, '', prefix, date.flatten, name, suffix)
+  end
+end
+
 module RulesHelpers
+  include RouteHelpers
+
+  def compile_for_item(item)
+    log("compiling #{item.identifier}")
+    extension = item[:extension].split('.').last
+    filter_for_extension!(extension).call
+  end
+
   def filter_for_extension!(extension)
     case extension
     when 'textile' then lambda { filter :redcloth }
     when 'haml'    then lambda { filter :haml, { :format => :html5 } }
     else
       raise ArgumentError.new("unmapped extension")
+    end
+  end
+
+  def layout_for(item, kind = nil)
+    kind ||= item.identifier.split('/').reject { |i| i.blank? }.first.to_sym
+    target = case kind
+    when :posts then 'main'
+    when :pages then 'home'
+    else             'home'
+    end
+
+    log("laying out #{target} (#{kind}) => #{item.identifier}")
+    layout target
+  end
+
+  def route_for(kind, item)
+    self.send(:"route_for_#{kind.to_s}", item).tap do |route|
+      log("routing for   #{item} (#{kind.to_s})")
+      log("  route components are: #{route.components}")
     end
   end
 end
